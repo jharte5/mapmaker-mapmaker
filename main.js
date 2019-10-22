98a7722d043d48f4b36900721fd6938bcfd16720
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-22 14:58:16 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function doubleAll() {
-}
+function doubleAll(arr, start = 0, end = arr.length) {
+  const newArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * 2)
+  }
+  return newArr
+} 
+
 
 function absoluteValues() {
+  const newArr =[]
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr)
+  }
+  return 
 }
 
 function yelledGreetings() {
